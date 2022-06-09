bfa172c181d8d00894a0128c6642ea7446101b5f
yunho <yunho>
2022-06-09 17:27:24 +0900

    컴포넌트 생성 연습

--- src/App.js
+++ src/App.js
@@ -3,15 +3,33 @@ import './App.css';
 
 function Header(){
   return <header>
-    <h1><a href="/">WEB</a></h1>
+    <h1><a href="/">Reat</a></h1>
   </header>
+}
+
+function Nav(){
+  return <nav>
+    <ol>
+      <li><a href='/read/1'>html</a></li>
+      <li><a href='/read/2'>css</a></li>
+      <li><a href='/read/3'>js</a></li>
+    </ol>
+  </nav>
+}
+
+function Article(){
+  return <article>
+  <h2>Welcome</h2>
+  Hello, React
+</article>
 }
 
 function App() {
   return (
-    <div class="Test">
+    <div>
       <Header></Header>
-      Hello React!!
+      <Nav></Nav>
+      <Article></Article>
     </div>
   );
 }
