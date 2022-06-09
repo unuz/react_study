3a86d787d59f8bcbde66476c1d9ead3d544b7956
yunho <yunho>
2022-06-09 17:21:37 +0900

    첫 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Header(){
+  return <header>
+    <h1><a href="/">WEB</a></h1>
+  </header>
+}
+
 function App() {
   return (
     <div class="Test">
+      <Header></Header>
       Hello React!!
     </div>
   );
