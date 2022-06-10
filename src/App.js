9cdd5a83726123f3c8b53ec0cb55e60a8a830e78
yunho <yunho>
2022-06-10 16:27:35 +0900

    update 추가

--- src/App.js
+++ src/App.js
@@ -62,6 +62,35 @@ function Create(props){
   </article>
 }
 
+//Update Component
+function Update(props){
+  const [title, setTitle] = useState(props.title);
+  const [body, setBody] = useState(props.body);
+  return <article>
+  <h2>Update</h2>
+  <form onSubmit={(event)=>{
+    event.preventDefault(); // submit 기본 이벤트 실행 X
+    const title = event.target.title.value;
+    const body = event.target.body.value;
+    props.onUpdate(title, body); // onUpdate 실행
+  }}>
+    <p>
+      <input type="text" name="title" placeholder='title을 입력하세요' value={title} onChange={event=>{
+        setTitle(event.target.value);
+      }}/>
+    </p>
+    <p>
+      <textarea name="body" placeholder='body를 입력하세요' value={body} onChange={event=>{
+        setBody(event.target.value);
+      }}/>
+    </p>
+    <p>
+      <input type="submit" value="update" />
+    </p>
+  </form>
+</article>
+}
+
 function App() {
   const [mode, setMode] = useState('WELCOME');
   const [id, setId] = useState(null);
@@ -72,6 +101,7 @@ function App() {
     {id:3, title:'javascript',body:'javascript is ...'}
   ]);
   let content = null;
+  let contextControl = null;
   if(mode === 'WELCOME'){
     content = <Article title="Welcom" body="Hello, React"></Article>
   }else if(mode === 'READ'){
@@ -84,16 +114,43 @@ function App() {
       }
     }
     content = <Article title={title} body={body}></Article>
+    contextControl = <li><a href={'/update/'+id} onClick={event=>{
+      event.preventDefault(); //a태그 기본 이벤트 실행 X
+      setMode('UPDATE'); //mode state 'UPDATE'로 변경
+    }}>Update</a></li>
   }else if(mode === 'CREATE'){
     content = <Create onCreate={(_title, _body)=>{
       const newTopic = {id:nextId, title:_title, body:_body};
       const newTopics = [...topics]; // topics 복제하여 사용 *객체는 복제하여 처리하여야 함
       newTopics.push(newTopic);
       setTopics(newTopics); // newTopics topics state set
-      setMode('READ');
-      setId(nextId);
-      setNextId(nextId+1);
+      setMode('READ'); //mode state 'READ'로 변경
+      setId(nextId);  //id state nextId로 변경
+      setNextId(nextId+1); //nextId state nextId+1로 변경
     }}></Create>
+  }else if(mode === 'UPDATE'){
+
+    let title, body = null;
+    for(let i=0; i< topics.length; i++){
+      console.log(topics[i].id, id);
+      if(topics[i].id === id){
+        title = topics[i].title;
+        body = topics[i].body;
+      }
+    }
+
+    content = <Update title={title} body={body} onUpdate={(title, body)=>{
+      const newTopics = [...topics]
+      const updatedTopic = {id: id, title: title, body: body}
+      for(let i=0; i<newTopics.length; i++){
+        if(newTopics[i].id === id){
+          newTopics[i] = updatedTopic;
+          break;
+        }
+      }
+      setTopics(newTopics);
+      setMode('READ');
+    }}></Update>
   }
   return (
     <div>
@@ -105,10 +162,15 @@ function App() {
         setId(id);
       }}></Nav>
       {content}
-      <a href='/create' onClick={(event)=>{
-        event.preventDefault(); // a 태그 기본 이벤트 실행 X
-        setMode('CREATE'); // mode state 'CREATE'로 변경
-      }}>Create</a>
+      <ul>
+        <li>
+        <a href='/create' onClick={(event)=>{
+          event.preventDefault(); // a 태그 기본 이벤트 실행 X
+          setMode('CREATE'); // mode state 'CREATE'로 변경
+        }}>Create</a>
+        </li>
+        {contextControl}
+      </ul>
     </div>
   );
 }
