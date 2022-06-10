1f13de67681f16c4f91687bfcb93069f574822ca
yunho <yunho>
2022-06-10 15:11:04 +0900

    component create

--- src/App.js
+++ src/App.js
@@ -39,14 +39,37 @@ function Article(props){
 </article>
 }
 
+function Create(props){
+  return <article>
+    <h2>Create</h2>
+    <form onSubmit={(event)=>{
+      event.preventDefault();
+      const title = event.target.title.value;
+      const body = event.target.body.value;
+      props.onCreate(title, body);
+    }}>
+      <p>
+        <input type="text" name="title" placeholder='title을 입력하세요'/>
+      </p>
+      <p>
+        <textarea name="body" placeholder='body를 입력하세요' />
+      </p>
+      <p>
+        <input type="submit" value="create" />
+      </p>
+    </form>
+  </article>
+}
+
 function App() {
   const [mode, setMode] = useState('WELCOME');
   const [id, setId] = useState(null);
-  const topics = [
+  const [nextId, setNextId] = useState(4);
+  const [topics, setTopics] = useState([
     {id:1, title:'html',body:'html is ...'},
     {id:2, title:'css',body:'css is ...'},
     {id:3, title:'javascript',body:'javascript is ...'}
-  ]
+  ]);
   let content = null;
   if(mode === 'WELCOME'){
     content = <Article title="Welcom" body="Hello, React"></Article>
@@ -60,6 +83,16 @@ function App() {
       }
     }
     content = <Article title={title} body={body}></Article>
+  }else if(mode === 'CREATE'){
+    content = <Create onCreate={(_title, _body)=>{
+      const newTopic = {id:nextId, title:_title, body:_body};
+      const newTopics = [...topics]; // topics 복제하여 사용 *객체는 복제하여 처리하여야 함
+      newTopics.push(newTopic);
+      setTopics(newTopics); // newTopics topics state set
+      setMode('READ');
+      setId(nextId);
+      setNextId(nextId+1);
+    }}></Create>
   }
   return (
     <div>
@@ -71,6 +104,10 @@ function App() {
         setId(id);
       }}></Nav> {/* 변수 전달 시 중괄호로 감싸줌 */}
       {content}
+      <a href='/create' onClick={(event)=>{
+        event.preventDefault();
+        setMode('CREATE')
+      }}>Create</a>
     </div>
   );
 }
